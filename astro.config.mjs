fdf7e96b7adc374554453ca4d65132e3526230ae
FelipeIzolan <80170121+FelipeIzolan@users.noreply.github.com>
2025-03-24 10:43:16 -0300

    Hello, World!

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import { resolve } from 'path';
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+import sitemap from "@astrojs/sitemap";
+
+const MdxConf = {
+  shikiConfig: {
+    theme: 'github-dark',
+    transformers: [
+      {
+        pre(node) {
+          node.properties.style = "background-color:#212121;color:#e1e4e8;overflow-x:auto;"
+        }
+      }
+    ]
+  },
+  rehypePlugins: [
+    rehypeKatex,
+  ],
+  remarkPlugins: [
+    remarkMath,
+  ],
+  gfm: true
+}
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://izolipe.com/',
+  prefetch: false,
+  integrations: [mdx(MdxConf), sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        '#': resolve(import.meta.dirname, './src'),
+        '@img': resolve(import.meta.dirname, './src/assets/imgs')
+      }
+    }
+  },
+  experimental: {
+    svg: true
+  }
+});
