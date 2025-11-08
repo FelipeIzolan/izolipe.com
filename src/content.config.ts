c7b340ae6b5570cebeb96709d4aff59a2941afd3
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2025-11-08 05:32:40 -0300

    update

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({
+    pattern: '**/*.{md,mdx}',
+    base: './src/content/blog'
+  }),
+  schema: z.object({
+    date: z.string().regex(/^\d{2}-\d{2}-\d{4}$/).optional(),
+    title: z.string(),
+    description: z.string(),
+    author: z.string().optional(),
+    katex: z.boolean().optional()
+  })
+});
+
+export const collections = { blog };
